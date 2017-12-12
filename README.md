061a80c2bfff61032c6cff2d6cea9783fc913aff
Amit K Biswas <biswas1992@hotmail.com>
2017-12-12 14:40:13 -0500

    Created thread controller to rendering thread to the view and implemented auth

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Test-Driven-Forum
 
 ##Feature
-#####1.Thread
-#####2.Reply 
-#####3.User 
+##Thread
+##Reply 
+##User 
 
-####Eloqunet 
+##Eloqunet 
 * thread is created by user 
 * reply is belongs to thread  and belongs to user 
 
---
+++ tests/Feature/ThreadsTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+
+class ThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+    /**
+     * A basic test example.
+     *
+     *
+     */
+    public function a_user_can_browse_threads()
+    {
+        $response = $this->get('/threads');
+
+        $response->assertStatus(200);
+    }
+}
